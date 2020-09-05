d4493c9e1bc3382019fe63fb7528e1140551ecf6
mishachatnoir <mihails.kosjanenko@gmail.com>
2020-09-05 20:27:55 +0300

    Initial commit

--- GenerateLogs.sh
+++ GenerateLogs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-touch -t 2020040100.00 app_health.5
-touch -t 2020050100.00 app_health.4
-touch -t 2020060100.00 app_health.3
-touch -t 2020070100.00 app_health.2
-touch -t 2020080100.00 app_health.1
-touch -t 2020090100.00 app_health.5
+mkdir log
+touch -t log/2020040100.00 app_health.5
+touch -t log/2020050100.00 app_health.4
+touch -t log/2020060100.00 app_health.3
+touch -t log/2020070100.00 app_health.2
+touch -t log/2020080100.00 app_health.1
+touch -t log/2020090100.00 app_health.5
 
